ebb6d03230fba46e9e96d7b44d2b36fb320c4864
tsanydzul <tsany.dzul@gmail.com>
2023-04-12 22:04:08 +0700

    create initial controller

--- src/main/java/com/accenture/test/springboot/controller/UserController.java
+++ src/main/java/com/accenture/test/springboot/controller/UserController.java
@@ -1,7 +1,18 @@
 package com.accenture.test.springboot.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("v1/users")
 public class UserController {
+
+    @GetMapping
+    public String getAllUsers(){
+        return "Hello World";
+    }
+
+    @PostMapping
+    public String createNewUser(){
+        return "Hello World Create";
+    }
 }
